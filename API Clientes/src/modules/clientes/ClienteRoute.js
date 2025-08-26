445b38b623a2fed3d10a3603a5fd7c61a83fa4e5
Cauê Alves <cauealvesb4@gmail.com>
2025-08-26 20:25:29 -0300

    Criando a base do Service

---
+++ API Clientes/src/modules/clientes/ClienteRoute.js
@@ -0,0 +1,21 @@
+
+
+export async function getClientes() {
+    return
+}
+
+export async function getClientesPeloID() {
+    return
+}
+
+export async function postCliente() {
+    return
+}
+
+export async function putCliente() {
+    return
+}
+
+export async function deleteCliente() {
+    return
+}
