608ac2c47c08ccc6ba65a8e028e9dbbb2e39aa82
Cauê Alves <cauealvesb4@gmail.com>
2025-08-26 20:58:56 -0300

    Mudando identação e finalziando controller

--- API Clientes/src/modules/clientes/ClienteController.js
+++ API Clientes/src/modules/clientes/ClienteController.js
@@ -1,119 +1,110 @@
-import * as ClienteService from './ClienteService.js'
-
-
+import * as ClienteService from "./ClienteService.js";
 
 export async function getClientes(req, res) {
-    try
-    {
-        const clientes = await ClienteService.getClientes()
-        if(clientes == null){
-            res.statusCode(200).json({
-                message: "Nenhum dado encontrado!"
-            });
-            return;
-        }
+  try {
+    const clientes = await ClienteService.getClientes();
+    if (clientes == null) {
+      res.statusCode(200).json({
+        message: "Nenhum dado encontrado!",
+      });
+      return;
+    }
 
-        res.json(clientes);
-    }
-    catch(ex) {
-        res.statusCode(500).json({
-            message: "Erro ao listar clientes.",
-            details: ex
-        })
-    }
+    res.json(clientes);
+  } catch (ex) {
+    res.statusCode(500).json({
+      message: "Erro ao listar clientes.",
+      details: ex,
+    });
+  }
 }
 
-export async function getClientesPeloID(idCliente) {
-    try
-    {
-        const cliente = await ClienteService.getClientesPeloID(idCliente);
-        if(cliente == null) {
-            res.statusCode(200).json({
-                message: "Nenhum cliente encontrado com esse id."
-            })
-            return;
-        }
+export async function getClientesPeloID(req, res) {
+  try {
+    const { idCliente } = req.body;
 
-        res.json(cliente);
+    const cliente = await ClienteService.getClientesPeloID(idCliente);
+    if (cliente == null) {
+      res.statusCode(200).json({
+        message: "Nenhum cliente encontrado com esse id.",
+      });
+      return;
     }
-    catch(ex) 
-    {
-        res.statusCode(500).json({
-            message: "Erro ao listar cliente.",
-            details: ex
-        })
-    }
+
+    res.json(cliente);
+  } catch (ex) {
+    res.statusCode(500).json({
+      message: "Erro ao listar cliente.",
+      details: ex,
+    });
+  }
 }
 
-export async function postCliente(dto) {
-    try
-    {
-        const resposta = await ClienteService.postCliente(dto);
-        if(resposta == null) {
-            res.statusCode(400).json({
-                message: "Ocorreu algum erro ao tentar criar um cliente."
-            })
-            return;
-        }
-        res.statusCode(201).json({
-            message: resposta
-        })
+export async function postCliente(req, res) {
+  try {
+    const { dto } = req.body;
+
+    const resposta = await ClienteService.postCliente(dto);
+    if (resposta == null) {
+      res.statusCode(400).json({
+        message: "Ocorreu algum erro ao tentar criar um cliente.",
+      });
+      return;
     }
-    catch (ex) 
-    {
-        res.statusCode(500).json({
-            message: "Erro ao criar cliente.",
-            details: ex
-        })
-    }
+    
+    res.statusCode(201).json({
+      message: resposta,
+    });
+  } catch (ex) {
+    res.statusCode(500).json({
+      message: "Erro ao criar cliente.",
+      details: ex,
+    });
+  }
 }
 
-export async function putCliente(dto, idCliente) {
-    try
-    {
-        const resposta = await ClienteService.putCliente(dto, idCliente);
-        if(resposta === null) {
-            res.statusCode(400).json({
-                message: "Ocorreu algum erro ao tentar alterar esse cliente."
-            });
-            return;
-        }
+export async function putCliente(req, res) {
+  try {
+    const { dto, idCliente } = req.body;
 
-        res.statsCode(200).json({
-            message: resposta
-        })
+    const resposta = await ClienteService.putCliente(dto, idCliente);
+    if (resposta === null) {
+      res.statusCode(400).json({
+        message: "Ocorreu algum erro ao tentar alterar esse cliente.",
+      });
+      return;
     }
-    catch(ex) 
-    {
-        res.statusCode(500).json({
-            message: "Erro ao alterar cliente.",
-            details: ex
-        })
-    }
+
+    res.statsCode(200).json({
+      message: resposta,
+    });
+  } catch (ex) {
+    res.statusCode(500).json({
+      message: "Erro ao alterar cliente.",
+      details: ex,
+    });
+  }
 }
 
 export async function deleteCliente(req, res) {
-    try
-    {
-        const { idCliente } = req.body;
+  try {
+    const { idCliente } = req.body;
 
-        const resposta = await ClienteService.deleteCliente(idCliente);
-        if(resposta === null) {
-            res.statsCode(400).json({
-                message: "Ocorreu um erro ao tentar alterar esse cliente."
-            })
-            return;
-        }
+    const resposta = await ClienteService.deleteCliente(idCliente);
+    if (resposta === null) {
+      res.statsCode(400).json({
+        message: "Ocorreu um erro ao tentar alterar esse cliente.",
+      });
+      return;
+    }
 
-        res.statsCode(200).json({
-            message: resposta
-        })
-    }
-    catch(ex) 
-    {
-        res.statusCode(500).json({
-            message: "Erro ao alterar cliente.",
-            details: ex
-        })
-    }
+    res.statsCode(200).json({
+      message: resposta,
+    });
+  } catch (ex) {
+    res.statusCode(500).json({
+      message: "Erro ao alterar cliente.",
+      details: ex,
+    });
+  }
 }
