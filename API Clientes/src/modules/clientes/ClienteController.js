6ceb76543173a8a7919228879139cdf84ba49414
Cauê Alves <cauealvesb4@gmail.com>
2025-08-26 20:37:37 -0300

    Incluindo funação para pesquisar por apenas um id

--- API Clientes/src/modules/clientes/ClienteController.js
+++ API Clientes/src/modules/clientes/ClienteController.js
@@ -7,7 +7,9 @@ export async function getClientes(req, res) {
     {
         const clientes = await ClienteService.getClientes()
         if(clientes == null){
-            res.statusCode(400).json({message: "Nenhum dado encontrado!"});
+            res.statusCode(200).json({
+                message: "Nenhum dado encontrado!"
+            });
         }
 
         res.json(clientes);
@@ -15,6 +17,27 @@ export async function getClientes(req, res) {
     catch(ex) {
         res.statusCode(500).json({
             message: "Erro ao listar clientes.",
+            details: ex
+        })
+    }
+}
+
+export async function getClientesPeloID(idCliente) {
+    try
+    {
+        const cliente = await ClienteService.getClientesPeloID(idCliente);
+        if(cliente == null) {
+            res.statusCode(200).json({
+                message: "Nenhum cliente encontrado com esse id."
+            })
+        }
+
+        res.json(cliente);
+    }
+    catch(ex) 
+    {
+        res.statusCode(500).json({
+            message: "Erro ao listar cliente.",
             details: ex
         })
     }
