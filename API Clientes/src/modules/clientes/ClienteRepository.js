cd498f22d57ba77b863ce7ba4fa0a2fd68546073
Cauê Alves <cauealvesb4@gmail.com>
2025-09-02 23:21:25 -0300

    Incluindo prisma para dar o select

--- API Clientes/src/modules/clientes/ClienteRepository.js
+++ API Clientes/src/modules/clientes/ClienteRepository.js
@@ -1,2 +1,13 @@
 import {PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient();
+
+export async function listarClientes() {
+    try 
+    {
+        return await prisma.user.findMany();
+    } catch (ex) 
+    {
+        console.log("Erro ao listar clientes.")
+        throw ex;
+    }
+}
